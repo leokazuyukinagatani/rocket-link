bad935bc3b3413fd5a04f8b76358ad2612a39738
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-07-06 23:20:48 -0300

    fix:vantajs

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const body = document.body;
 const iconLight = document.querySelector('.icon-light');
 const iconDark = document.querySelector('.icon-dark');
+const vanta = document.querySelector('#vanta');
 
 function changeMode() {
   body.classList.toggle("dark-blue");
   iconLight.classList.toggle("no-display");
   iconDark.classList.toggle("no-display");
+  vanta.classList.toggle("no-display");
 }
 
 
