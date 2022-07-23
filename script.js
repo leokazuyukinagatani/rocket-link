6362468d79421c4c26bbb6e9d035c5d401dca460
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-07-23 14:16:15 -0300

    feature:

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const body = document.body;
 const iconLight = document.querySelector('.icon-light');
 const iconDark = document.querySelector('.icon-dark');
+const vantaDark = document.querySelector('#vanta-dark');
 const vanta = document.querySelector('#vanta');
+
 
 function changeMode() {
   body.classList.toggle("dark-blue");
   iconLight.classList.toggle("no-display");
   iconDark.classList.toggle("no-display");
+  vantaDark.classList.toggle("no-display");
   vanta.classList.toggle("no-display");
+
 }
 
 
